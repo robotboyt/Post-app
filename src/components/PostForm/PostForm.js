30cb33f8f7f15adcbc0adc1cc58b6bf895e25b0d
robotboyt <bokachodantes@gmail.com>
2022-03-23 11:20:25 +0200

    Initialize project using Create React App

---
+++ src/components/PostForm/PostForm.js
@@ -0,0 +1,22 @@
+// import React, { useState } from 'react'
+// import './postForm.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faClose } from '@fortawesome/free-solid-svg-icons'
+
+// function PostForm() {
+//     return(
+//         <div>
+//             <form onSubmit={onSubmit}> 
+//                 <label>Write Title</label>
+//                 <input type="text" name='Title' value={title} required placeholder='Title' onChange={(e) => setTitle(e.target.value)}></input>
+//                 <br/>
+//                 <label>Write Description</label>
+//                 <input type="text" name='Description' value={description} required placeholder='description' onChange={(e) => setDescription(e.target.value)}></input>
+//                 <br/>
+//                 <button className='modal-btn' type='submit'>Add</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export  default PostForm
